2f826df2a8842d8fc8d650bbd961d083bc9dd82a
Ioanna-Maria Panagou <ipanagou@uth.gr>
2024-04-09 14:21:10 +0300

    updated platform in util file

---
+++ envs.sh
@@ -0,0 +1,4 @@
+export FINN_XILINX_PATH=/opt/xilinx
+export FINN_XILINX_VERSION=2023.1
+export FINN_DOCKER_EXTRA="-v /srv/homes/ipanagou/thesis/code:/srv/homes/ipanagou/thesis/finn/thesis/code "
+export FINN_HOST_BUILD_DIR="/srv/homes/ipanagou/thesis/finn_dev_ipanagou"
--- src/finn/util/basic.py
+++ src/finn/util/basic.py
@@ -73,7 +73,7 @@ alveo_part_map["U55C"] = "xcu55c-fsvh2892-2L-e"
 alveo_default_platform = dict()
 alveo_default_platform["U50"] = "xilinx_u50_gen3x16_xdma_5_202210_1"
 alveo_default_platform["U200"] = "xilinx_u200_gen3x16_xdma_2_202110_1"
-alveo_default_platform["U250"] = "xilinx_u250_gen3x16_xdma_2_1_202010_1"
+alveo_default_platform["U250"] = "xilinx_u250_gen3x16_xdma_4_1_202210_1"
 alveo_default_platform["U280"] = "xilinx_u280_gen3x16_xdma_1_202211_1"
 alveo_default_platform["U55C"] = "xilinx_u55c_gen3x16_xdma_3_202210_1"
 
